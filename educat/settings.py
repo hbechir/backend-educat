4cee8c71ac2dd00b086f1ea11496814bcd1fc278
bechir hamdi <hbechir52@gmail.com>
2024-04-10 17:11:48 +0100

    Update ALLOWED_HOSTS to allow all hosts in settings.py

--- educat/settings.py
+++ educat/settings.py
@@ -28,14 +28,7 @@ DEBUG = True
 
 
 CURRENT_HOST_FROM_ENV = os.getenv('HOST')
-ALLOWED_HOSTS = [
-    '.vercel.app',
-    '127.0.0.1',
-    'localhost'
-    'now.sh',
-    'educatbackend150802.up.railway.app',
-    
-]
+ALLOWED_HOSTS = ["*"]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
@@ -179,11 +172,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
