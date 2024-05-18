959ac590bbad253502c6d013fe9c5333ed089e44
ferdaous sarhani <hbechir52@gmail.com>
2024-05-18 20:26:12 +0100

    chore: Update database configuration with environment variables

--- educat/settings.py
+++ educat/settings.py
@@ -119,14 +119,34 @@ WSGI_APPLICATION = 'educat.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# import from .env 
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+# DB_HOST=viaduct.proxy.rlwy.net
+# DB_NAME=railway
+# DB_PASSWORD=gQyUUkDiamzqCezkmhIyNoCzHBWVJKyu
+# DB_PORT=28393
+# DB_USER=postgres
+
+db_host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
+db_password = os.getenv('DB_PASSWORD')
+db_port = os.getenv('DB_PORT')
+db_user = os.getenv('DB_USER')
+
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'FcChzWAKQznpHsbmRigvINEyLvHKvyFe',
-        'HOST': 'viaduct.proxy.rlwy.net',
-        'PORT': '52205',
+        'NAME': db_name,
+        'USER': db_user,
+        'PASSWORD': db_password,
+        'HOST': db_host,
+        'PORT':db_port,
     }
 }
 
